4aef1fad82c76e382f5e0a889e5449814500e88a
Yordanos tibebu <yordanostibebu5@gmail.com>
2024-02-03 14:03:42 -0800

    Added first commit for netflix clone

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import "./Banner.css"
+import axios from "../../axios"
+import requests from "../../requests"
+
+function Banner() {
+  return (
+    <div>
+      <h1>banner</h1>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Components/Row/Row.jsx
@@ -0,0 +1,35 @@
+import "./Row.css";
+import axios from "../../axios";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${isLargeRow ? movie.poster_path: movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
